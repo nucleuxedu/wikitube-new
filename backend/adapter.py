b4f92c9e57bc5b3273b99f61d900c5371725cf43
KATARI REVATHI <katari.revathi@gmail.com>
2025-02-06 11:56:25 +0530

    callbackviewchanged"

--- backend/adapter.py
+++ backend/adapter.py
@@ -35,22 +35,22 @@ from django.contrib.auth import get_user_model
 import random
 
 
-# class MyAccountAdapter(DefaultAccountAdapter):
-#     def is_open_for_signup(self, request):
-#         """Allow social signups without manual approval"""
-#         return True
+class MyAccountAdapter(DefaultAccountAdapter):
+    def is_open_for_signup(self, request):
+        """Allow social signups without manual approval"""
+        return True
 
-#     def user_exists(self, username):
-#         """Check if a user with the given username already exists"""
-#         User = get_user_model()
-#         return User.objects.filter(username=username).exists()
+    def user_exists(self, username):
+        """Check if a user with the given username already exists"""
+        User = get_user_model()
+        return User.objects.filter(username=username).exists()
 
-#     def generate_unique_username(self, base_username):
-#         """Generate a unique username if needed"""
-#         new_username = slugify(base_username)
-#         while self.user_exists(new_username):
-#             new_username = f"{new_username}{random.randint(1000, 9999)}"
-#         return new_username
+    def generate_unique_username(self, base_username):
+        """Generate a unique username if needed"""
+        new_username = slugify(base_username)
+        while self.user_exists(new_username):
+            new_username = f"{new_username}{random.randint(1000, 9999)}"
+        return new_username
 
 # class MySocialAccountAdapter(DefaultSocialAccountAdapter):
 #     def populate_user(self, request, sociallogin, data):
